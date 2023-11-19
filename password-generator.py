75061ba623b77b5de5e461ae0251840ac78caf35
Vicente Mendez <vicentem@vmhq.cl>
2023-11-18 22:32:55 -0300

    update code

---
+++ password-generator.py
@@ -0,0 +1,20 @@
+import random
+
+def generar_contrasena(longitud, usar_mayusculas=True, usar_numeros=True, usar_simbolos=True):
+    letras_minusculas = 'abcdefghijklmnopqrstuvwxyz'
+    letras_mayusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' if usar_mayusculas else ''
+    numeros = '0123456789' if usar_numeros else ''
+    simbolos = '!@#$%^&*()-_=' if usar_simbolos else ''
+    
+    # Combinando todos los caracteres posibles
+    caracteres_posibles = letras_minusculas + letras_mayusculas + numeros + simbolos
+
+    # Generando la contraseña
+    contrasena = ''.join(random.choice(caracteres_posibles) for i in range(longitud))
+    
+    return contrasena
+
+# Interacción con el usuario
+longitud = int(input("Longitud de la contraseña: "))
+contrasena = generar_contrasena(longitud)
+print("Tu nueva contraseña es:", contrasena)
