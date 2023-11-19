12e58de0c83d34c984f63b508ae149afe994dbaa
Vicente Mendez <vicentem@vmhq.cl>
2023-11-19 00:39:04 -0300

    update code

--- password-generator.py
+++ password-generator.py
@@ -1,27 +1,46 @@
-import random
+'''Password Generator'''
+
+import secrets
 
 
 def generate_password(length, use_uppercase=True, use_numbers=True, use_symbols=True):
-    lowercase_letters = 'abcdefghijklmnopqrstuvwxyz'
-    uppercase_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' if use_uppercase else ''
-    numbers = '0123456789' if use_numbers else ''
-    symbols = '!@#$%^&*()-_=' if use_symbols else ''
+    lowercase_letters = 'abcdefghijkmnopqrstuvwxyz'  # excluding 'l'
+    uppercase_letters = 'ABCDEFGHJKLMNPQRSTUVWXYZ'  # excluding 'I' and 'O'
+    numbers = '23456789'  # excluding '0' and '1'
+    symbols = '!@#$%^&*()-_='
     
-    # Combining all possible characters
-    possible_characters = lowercase_letters + uppercase_letters + numbers + symbols
+    possible_characters = lowercase_letters
+    if use_uppercase:
+        possible_characters += uppercase_letters
+    if use_numbers:
+        possible_characters += numbers
+    if use_symbols:
+        possible_characters += symbols
 
-    # Generating the password
-    password = ''.join(random.choice(possible_characters) for i in range(length))
-    
+    password = ''.join(secrets.choice(possible_characters) for i in range(length))
     return password
 
 
-# User interaction
-def main():
-    length = int(input("Password length: "))
-    password = generate_password(length)
+def user_pass():
+    while True:
+        try:
+            length = int(input("Password length: "))
+            if length <= 0:
+                raise ValueError
+            break
+        except ValueError:
+            print("Please enter a positive integer.")
+
+    use_uppercase = input("Do you want to include uppercase letters? (y/n): ").lower() == 'y'
+    use_numbers = input("Do you want to include numbers? (y/n): ").lower() == 'y'
+    use_symbols = input("Do you want to include symbols? (y/n): ").lower() == 'y'
+
+    password = generate_password(length, use_uppercase, use_numbers, use_symbols)
     print("Your new password is:", password)
 
+
+def main():
+    user_pass()
 
 if __name__ == "__main__":
     main()
